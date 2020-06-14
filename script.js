678453626b0592cecee3f0ab5e697680eff36f36
martinagabrielli <martina.gabrielli@yahoo.it>
2020-06-14 15:24:14 +0100

    added copy to clipboard function

--- script.js
+++ script.js
@@ -7,7 +7,14 @@ const hexOptions = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 const hexBtn = document.querySelector('button')
 const body = document.querySelector('body')
 const hexHeading = document.querySelector('h2')
-const copyBtn = document.querySelector('[data-copy-btn]')
+const hexCode = document.querySelector('[data-color-code]')
+
+const copyBtn = document.querySelector('[data-hexcode]')
+
+// body.appendChild(copyBtn)
+
+// copyBtn.textContent = 'Copy Color'
+// copyBtn.dataset.hexCode = 'hexcode'
 
 // button event
 
@@ -17,22 +24,21 @@ let changeColor = () => {
         randomCode += hexOptions[Math.floor(Math.random() * hexOptions.length)]
     }
     body.style.background = `#${randomCode}`
-    hexHeading.innerText = `This Hex Color Code Is: #${randomCode}`
+    hexCode.style.color = `#${randomCode}`
+    hexCode.value = `#${randomCode}`
 }
 
 // function for copy to clipboard option
 
-function copyHexColor(){
-    let hexCode = document.querySelector('[data-hexcode]')
-    let range = document.createRange()
-    range.selectNode(hexCode)
-    window.getSelection().addRange(range)
-    document.execCommand('copy');
-    alert(`Color ${hexCode.textContent} copied to clipboard`)
+const copyToClipboard = () => {
+    let copyText = document.querySelector('[data-color-code]')
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand("copy");
+    alert(`Color ${copyText.value} copied to clipboard!`);
 }
-
 
 // events
 
 hexBtn.addEventListener('click', changeColor)
-copyBtn.addEventListener('click', copyHexColor)
+copyBtn.addEventListener('click', copyToClipboard)
